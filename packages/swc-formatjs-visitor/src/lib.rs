45dc521b5c86b5aaa1352c475dd45e8d1af760d8
OJ Kwon <kwon.ohjoong@gmail.com>
2022-09-17 12:34:55 -0700

    test(mocha): initial setup

---
+++ packages/swc-formatjs-visitor/src/lib.rs
@@ -0,0 +1,11 @@
+use swc_core::{
+    ecma::visit::VisitMut,
+};
+
+pub struct FormatJSVisitor {}
+
+impl VisitMut for FormatJSVisitor {}
+
+pub fn create_formatjs_visitor() -> FormatJSVisitor {
+    FormatJSVisitor {}
+}
