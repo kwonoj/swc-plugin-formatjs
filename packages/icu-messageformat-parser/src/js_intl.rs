aeaeab1f8c1a5114720b50782b4817e29fa5b6e8
OJ Kwon <kwon.ohjoong@gmail.com>
2022-09-17 17:27:13 -0700

    feat(js_intl): define numberformatoptions

--- packages/icu-messageformat-parser/src/js_intl.rs
+++ packages/icu-messageformat-parser/src/js_intl.rs
@@ -1,10 +1,135 @@
 use serde::Serialize;
 
+#[derive(Clone, Debug, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum CompactDisplay {
+    Short,
+    Long,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum Notation {
+    Standard,
+    Scientific,
+    Engineering,
+    Compact,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum UnitDisplay {
+    Short,
+    Long,
+    Narrow,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum NumberFormatOptionsTrailingZeroDisplay {
+    Auto,
+    StripIfInteger,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum NumberFormatOptionsRoundingPriority {
+    Auto,
+    MorePrecision,
+    LessPrecision,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum NumberFormatOptionsLocaleMatcher {
+    Lookup,
+    #[serde(rename = "best fit")]
+    BestFit,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum NumberFormatOptionsStyle {
+    Decimal,
+    Percent,
+    Currency,
+    Unit,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum NumberFormatOptionsCurrencyDisplay {
+    Symbol,
+    Code,
+    Name,
+    NarrowSymbol,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum NumberFormatOptionsCurrencySign {
+    Standard,
+    Accounting,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum NumberFormatOptionsSignDisplay {
+    Auto,
+    Always,
+    Never,
+    ExceptZero,
+}
+
 /// Subset of options that will be parsed from the ICU message number skeleton.
-#[derive(Default, Clone, Debug, Eq, PartialEq, Serialize)]
+#[derive(Default, Clone, Debug, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct JsIntlNumberFormatOptions {
-    // TODO
+    // NumberFormatDigitOptions
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub minimum_integer_digits: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub minimum_significant_digits: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub maximum_significant_digits: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub minimum_fraction_digits: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub maximum_fraction_digits: Option<u32>,
+
+    // Intl.NumberFormatOptions
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub notation: Option<Notation>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub compact_display: Option<CompactDisplay>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub unit: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub unit_display: Option<UnitDisplay>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub locale_matcher: Option<NumberFormatOptionsLocaleMatcher>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub style: Option<NumberFormatOptionsStyle>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub currency_display: Option<NumberFormatOptionsCurrencyDisplay>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub currency_sign: Option<NumberFormatOptionsCurrencySign>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub sign_display: Option<NumberFormatOptionsSignDisplay>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub numbering_system: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub trailing_zero_display: Option<NumberFormatOptionsTrailingZeroDisplay>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub rounding_priority: Option<NumberFormatOptionsRoundingPriority>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub scale: Option<f32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub currency: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub use_grouping: Option<bool>,
 }
 
 /// Subset of options that will be parsed from the ICU message daet or time skeleton.
