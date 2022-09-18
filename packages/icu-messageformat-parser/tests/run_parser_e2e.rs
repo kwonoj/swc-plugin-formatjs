6c3be0b30af0a48e48b1054eae6351b918275dd6
OJ Kwon <kwon.ohjoong@gmail.com>
2022-09-17 17:43:45 -0700

    test(js_intl): update test cases

--- packages/icu-messageformat-parser/tests/run_parser_e2e.rs
+++ packages/icu-messageformat-parser/tests/run_parser_e2e.rs
@@ -57,7 +57,6 @@ fn read_sections<'a>(file: PathBuf) -> TestFixtureSections {
 #[fixture("tests/fixtures/number_skeleton_11")]
 #[fixture("tests/fixtures/number_skeleton_12")]
 #[fixture("tests/fixtures/number_skeleton_2")]
-#[fixture("tests/fixtures/number_skeleton_3")]
 #[fixture("tests/fixtures/number_skeleton_4")]
 #[fixture("tests/fixtures/number_skeleton_5")]
 #[fixture("tests/fixtures/number_skeleton_6")]
@@ -139,6 +138,8 @@ fn tests_skipped(_file: PathBuf) {
 #[fixture("tests/fixtures/quoted_string_4")]
 #[fixture("tests/fixtures/quoted_string_5")]
 #[fixture("tests/fixtures/number_skeleton_1")]
+
+#[fixture("tests/fixtures/number_skeleton_3")]
 fn parser_tests(file: PathBuf) {
     let fixture_sections = read_sections(file);
     let mut parser = Parser::new(
